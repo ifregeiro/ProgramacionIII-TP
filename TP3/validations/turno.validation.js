19418bfa1793e1fbc1846262d3aa8d582eb54681
Ignacio Fregeiro <114210301+ifregeiro@users.noreply.github.com>
2025-06-08 20:19:24 -0300

    Delete TP3/validations directory

--- TP3/validations/turno.validation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const turnoSchema = Joi.object({
-  fecha: Joi.date().required(),
-  hora: Joi.string().required(),
-  pacienteId: Joi.number().integer().required()
-});
-
-module.exports = { turnoSchema };
